d8b271ab874f15a237f141b156b943d33f9967d1
BreatheOfLove <110792517+BreatheOfLove@users.noreply.github.com>
2024-12-05 20:16:32 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,115 +1,10 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+#include "classMusic.h"
+#include "classMovies.h"
+#include "classSerials.h"
+#include "classBooks.h"
 using namespace std;
-
-class Movies {
-private:
-    string name;
-    string genre;
-    string country;
-    int durationMin;
-    int YearRelease;
-
-public:
-    Movies(string name, string genre, string country, int durationMin,
-        int YearRelease) {
-        this->name = name;
-        this->genre = genre;
-        this->country = country;
-        this->durationMin = durationMin;
-        this->YearRelease = YearRelease;
-    }
-    void print() const {
-        cout << "Название фильма: " << name << endl;
-        cout << "Жанр: " << genre << endl;
-        cout << "Страна: " << country << endl;
-        cout << "Длительность: " << durationMin << " минут" << endl;
-        cout << "Год выпуска: " << YearRelease << endl;
-    }
-};
-
-class Serials {
-private:
-    string name;
-    string genre;
-    string country;
-    int seasons;
-    int series;
-    int YearRelease;
-
-public:
-     Serials(string name, string genre, string country, int seasons, int series,
-        int YearRelease) {
-        this->name = name;
-        this->genre = genre;
-        this->country = country;
-        this->seasons = seasons;
-        this->series = series;
-        this->YearRelease = YearRelease;
-    }
-    void print() const {
-        cout << "Название фильма: " << name << endl;
-        cout << "Жанр: " << genre << endl;
-        cout << "Страна: " << country << endl;
-        cout << "Сезонов: " << seasons << " минут" << endl;
-        cout << "Серий в 1 сезоне: " << series << endl;
-        cout << "Год выпуска: " << YearRelease << endl;
-    }
-};
-
-class Music {
-private:
-    string name;
-    string executor;
-    string album;
-    string language;
-    int timeSec;
-    int YearRelease;
-public:
-    Music(string name, string executor, string album, string language, int timeSec, int YearRelease) {
-        this->name = name;
-        this->executor = executor;
-        this->album = album;
-        this->language = language;
-        this->timeSec = timeSec;
-        this->YearRelease = YearRelease;
-    }
-    void print() const {
-        cout << "Трек: " << name << endl;
-        cout << "Исполнитель: " << executor << endl;
-        cout << "Альбом: " << album << endl;
-        cout << "Язык: " << language << endl;
-        cout << "Длительность(сек): " << timeSec << endl;
-        cout << "Год выпуска: " << YearRelease << endl;
-    }
-};
-
-class Books {
-private:
-    string name;
-    string genre;
-    string author;
-    string language;
-    int pages;
-    int YearRelease;
-public:
-    Books(string name, string genre , string author, string language, int pages, int YearRelease) {
-        this->name = name;
-        this->genre = genre;
-        this->author = author;
-        this->language = language;
-        this->pages = pages;
-        this->YearRelease = YearRelease;
-    }
-    void print() const {
-        cout << "Название книги: " << name << endl;
-        cout << "Жанр: " << genre << endl;
-        cout << "Автор: " << author << endl;
-        cout << "Язык: " << language << endl;
-        cout << "Страниц: " << pages << endl;
-        cout << "Год выпуска: " << YearRelease << endl;
-    }
-};
 
 class OnlineArt : protected Movies,
     protected Serials,
@@ -190,4 +85,3 @@ int main() {
         }
     } while (act != 0);
 }
-
